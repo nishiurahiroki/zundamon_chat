90d366d28fdb80abf2e8bba860b109040e12570b
nishiura-hiroki <nishiurahiroki@gmail.com>
2023-04-02 22:56:39 +0900

    Talk and displayed Zundamon's answer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
 import IndexPage from '../components/pages/IndexPage';
+import { headers } from 'next/headers';
 
-type Props = { searchParams: { question: string; }; }
-
-function getAnswer(question : string) {
-  // TODO
+type Props = { 
+  searchParams: { question: string; };
+  headers : {
+    host : string;
+  }
 }
 
-export default function Page(props: Props) {
-  // console.log(props.searchParams.question)
+async function getAnswer(question : string, host : string) {
+  const params = {
+    question
+  }
+
+  const queryParams = new URLSearchParams(params); 
+  const response = await fetch(`http://${host}/api/question?${queryParams}`)
+  const json = await response.json()
+
+  return json.answer.content
+}
+
+export default async function Page(props: Props) {
+  const headersList = headers();
+
+  const answer = props.searchParams.question 
+                  ? await getAnswer(props.searchParams.question, headersList.get('host')) : ''
 
   return (
-    <IndexPage />
+    <>
+      <IndexPage />
+      <div>{answer}</div>
+    </>
   )
 }
