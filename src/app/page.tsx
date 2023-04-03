11e3d0d8a6a5574a602559b03a4195ede0539712
nishiura-hiroki <nishiurahiroki@gmail.com>
2023-04-03 22:20:47 +0900

    Fix api host get process.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import IndexPage from '../components/pages/IndexPage';
-import { headers } from 'next/headers';
 
 type Props = { 
   searchParams: { question: string; };
@@ -8,23 +7,21 @@ type Props = {
   }
 }
 
-async function getAnswer(question : string, host : string) {
+async function getAnswer(question : string) {
   const params = {
     question
   }
 
   const queryParams = new URLSearchParams(params); 
-  const response = await fetch(`http://${host}/api/question?${queryParams}`)
+  const response = await fetch(`${process.env.API_HOST}/api/question?${queryParams}`)
   const json = await response.json()
 
   return json.answer.content
 }
 
 export default async function Page(props: Props) {
-  const headersList = headers();
-
   const answer = props.searchParams.question 
-                  ? await getAnswer(props.searchParams.question, headersList.get('host')) : ''
+                  ? await getAnswer(props.searchParams.question) : ''
 
   return (
     <>
